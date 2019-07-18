d3e896ef0f910c0c856bb51accec913a0d805eab
milinsky <mail@milinsky.ru>
2019-07-18 23:49:03 +1000

    implementation routing Result class

--- src/Result.php
+++ src/Result.php
@@ -4,7 +4,18 @@ declare(strict_types=1);
 
 namespace Marussia\Router;
 
-class Result
+class Result implements \ArrayAccess
 {
+    protected $status = false;
 
+    public $handler = '';
+    
+    public $action = '';
+    
+    public $attributes = [];
+    
+    public function status() : bool
+    {
+        return $this->status;
+    }
 }
